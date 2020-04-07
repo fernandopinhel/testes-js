fab923c219cae886d9400dcb79ecb8c4a540e314
Fernando Pinhel <fernandopinhelll@gmail.com>
2020-04-07 11:23:53 -0300

    Corrigindo criação de id´s dinâmicas.

--- js/script.js
+++ js/script.js
@@ -21,36 +21,71 @@
 
 
 
+// function dispararLoop(){
+//     for(i = 1; i < 2; i++){
+        
+//         let criarDiv = document.createElement('div')
+        
+//         let pegarCol = document.querySelector('.col')
+
+//         let id = document.createAttribute('id')
+
+//         id.value = 'div-' + i
+        
+//         criarDiv.setAttributeNode(id)
+        
+//         pegarCol.appendChild(criarDiv)
+        
+//         criarDiv.className = "d1"
+        
+//         let criarParagrafo = document.createElement('p')
+        
+//         criarDiv.appendChild(criarParagrafo)
+        
+//         function Pessoa(nome, idade, sexo, profissao){
+//             this.nome = nome;
+//             this.idade = idade;
+//             this.sexo = sexo;
+//             this.profissao = profissao;
+//         }
+
+//         let fernando = new Pessoa("Fernando", "28", "Masculino", "Dev")
+
+//         criarParagrafo.innerHTML = fernando.nome + " " + fernando.idade + " " + fernando.sexo + " " + fernando.profissao
+//     }   
+// }
+
+
+let cont = 0
+
 function dispararLoop(){
-    for(i = 1; i < 2; i++){
-        
-        let criarDiv = document.createElement('div')
-        
-        let pegarCol = document.querySelector('.col')
+    let criarDiv = document.createElement('div')
 
-        let id = document.createAttribute('id')
+    let pegarCol = document.querySelector('.col')
 
-        id.value = 'div-' + i
-        
-        criarDiv.setAttributeNode(id)
-        
-        pegarCol.appendChild(criarDiv)
-        
-        criarDiv.className = "d1"
-        
-        let criarParagrafo = document.createElement('p')
-        
-        criarDiv.appendChild(criarParagrafo)
-        
-        function Pessoa(nome, idade, sexo, profissao){
-            this.nome = nome;
-            this.idade = idade;
-            this.sexo = sexo;
-            this.profissao = profissao;
-        }
+    let id = document.createAttribute('id')
 
-        let fernando = new Pessoa("Fernando", "28", "Masculino", "Dev")
+    cont++    
+    id.value = 'div-' + (cont)
 
-        criarParagrafo.innerHTML = fernando.nome + " " + fernando.idade + " " + fernando.sexo + " " + fernando.profissao
-    }   
+    criarDiv.setAttributeNode(id)
+
+    pegarCol.appendChild(criarDiv)
+
+    criarDiv.className = "d1"
+
+    let criarParagrafo = document.createElement('p')
+
+    criarDiv.appendChild(criarParagrafo)
+
+    function Pessoa(nome, idade, sexo, profissao){
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.profissao = profissao;
+    }
+
+    let fernando = new Pessoa("Fernando", "28", "Masculino", "Dev")
+
+    criarParagrafo.innerHTML = fernando.nome + " " + fernando.idade + " " + fernando.sexo + " " + fernando.profissao   
 }
